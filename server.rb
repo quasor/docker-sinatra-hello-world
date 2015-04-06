c16b9bba988b25fb0bf5d5678262a6bb88071427
Andrew Coldham <quasor@me.com>
2015-04-06 14:23:23 -0700

    build from remote

--- server.rb
+++ server.rb
@@ -1,10 +1,14 @@
 require 'sinatra'
- 
+
 #Bound to this address so that external hosts can access it, VERY IMPORTANT!
 set :bind, '0.0.0.0'
- 
+
 set :logging, true
- 
+
 get '/' do
-  'hello world from docker!'
+  'hello world from docker! ' + Time.now.to_s
+end
+
+get '/health' do
+  'I am healthy!' + Time.now.to_s
 end
